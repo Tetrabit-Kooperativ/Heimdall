40dc8b5ca138bd97673b99bbdabb88d5abee6ff3
Adrian Bratlann <aeb@tetrabit.coop>
2022-09-29 15:04:39 +0200

    playing around

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,3 +1,4 @@
 import { io } from 'socket.io-client'
+import { dev } from '$app/environment'
 
-export const socket = io('http://localhost:5173')
+export const socket = io(dev ? 'http://localhost:5173' : 'https://heimdall.tetrabit.coop')
