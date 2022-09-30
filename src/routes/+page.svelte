64b0db2778736767366ffeeb005793e506645cdf
Adrian Bratlann <aeb@tetrabit.coop>
2022-09-30 12:13:10 +0200

    bugfix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,7 +66,7 @@
 
     <form on:submit|preventDefault={sendMessage}>
         <input autocomplete=off bind:value={msg} name="message" type="text" placeholder="Skriv en besked til alle deltagere">
-        <input type="hidden" name="username" value={$page.data.user.name} />
+        <input type="hidden" name="username" value={$page.data?.user?.name} />
         <button>
             <svg width="28px" height="28px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M22 12L3 20l3.563-8L3 4l19 8zM6.5 12H22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
         </button>
